3711ccf42c3054e54dd2825f36ff879cf4f52a53
Jack Buehner <jackb@furman.edu>
2025-12-11 11:16:50 -0500

    chore: add default env value for VITE_MAP_SERVER_URL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,27 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      '$lib/navigation': path.resolve(__dirname, './src/lib/navigation/index.ts'),
-      '$lib/navigation/*': path.resolve(__dirname, './src/lib/navigation/*'),
-      '$lib/utils': path.resolve(__dirname, './src/lib/utils/index.ts'),
-      '$lib/utils/*': path.resolve(__dirname, './src/lib/utils/*'),
-      $lib: path.resolve(__dirname, './src/lib'),
-      '$lib/*': path.resolve(__dirname, './src/lib/*'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      'import.meta.env.VITE_MAP_SERVER_URL': JSON.stringify(
+        env.VITE_MAP_SERVER_URL || 'http://localhost:3000'
+      ),
     },
-  },
+    plugins: [svelte()],
+    resolve: {
+      alias: {
+        '$lib/navigation': path.resolve(__dirname, './src/lib/navigation/index.ts'),
+        '$lib/navigation/*': path.resolve(__dirname, './src/lib/navigation/*'),
+        '$lib/utils': path.resolve(__dirname, './src/lib/utils/index.ts'),
+        '$lib/utils/*': path.resolve(__dirname, './src/lib/utils/*'),
+        $lib: path.resolve(__dirname, './src/lib'),
+        '$lib/*': path.resolve(__dirname, './src/lib/*'),
+      },
+    },
+  };
 });
